ce48423fc83eba25c306d56d7f9fa325678c435b
AlekseyK <aleksey.kabishau@gmail.com>
2019-04-28 21:27:49 -0400

    Initial Commit

---
+++ HWS_Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS_Project7
+//
+//  Created by Aleksey on 0428..19.
+//  Copyright © 2019 Aleksey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
